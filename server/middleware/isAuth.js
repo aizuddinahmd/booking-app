6ab26b455775936f2e3f2d80c82acbc07f2ed064
Ahmad Aizuddin <aizuddinahmd0408@gmail.com>
2024-11-28 21:45:03 +0800

    add isAuth in middleware

---
+++ server/middleware/isAuth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+
+const isAuth = (req, res, next) => {
+  const bearerToken = req.headers.authorization;
+  const token = bearerToken.split(" ")[1];
+
+  // check if token is provided
+
+  if (!token) {
+    return res.status(401).json({
+      error: "Unauthorized",
+    });
+  }
+
+  const secretKey = "supersecret";
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        message: "Invalid token",
+      });
+    }
+    // pass user id to the next middleware
+    req.userId = decoded.id;
+    req.userEmail = decoded.email;
+    next();
+  });
+};
+export default isAuth;
